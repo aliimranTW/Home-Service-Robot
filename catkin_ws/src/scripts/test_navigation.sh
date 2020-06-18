aef3806a7565bec40e2bc40921f8c16101f8508a
aliimranTW <aliimrantw@gmail.com>
2020-06-19 00:45:36 +0500

    modified the project for my_robot instead of turtlebot. SLAM parameters tuned. Localization parameters and map to be modified.

---
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+xterm -e "pip install rospkg"
+
+sleep 5
+
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch my_robot world.launch" &
+
+sleep 5
+
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch my_robot amcl.launch"
+
+#sleep 5
+
+#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-xterm -e "pip install rospkg"
+#xterm -e "pip install rospkg"
+
+#sleep 5
+
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch my_robot world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/corridor.world" &
 
 sleep 5
 
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/corridor.world" &
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch gmapping slam_gmapping_pr2.launch" &
+
+#sleep 5
+
+#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 
 sleep 5
 
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
-
-sleep 5
-
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-
-sleep 5
-
-xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
