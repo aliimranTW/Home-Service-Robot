188040062a5c66356280e1e1e94af3e4f730d1fe
aliimranTW <aliimrantw@gmail.com>
2020-06-19 15:54:39 +0500

    tuned map parameters. problem in avoinding obstacles

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -4,8 +4,8 @@ xterm -e "pip install rospkg"
 
 sleep 5
 
+#xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch my_robot world.launch " &
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch my_robot world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/corridor.world" &
-
 sleep 5
 
 xterm -e "source /home/workspace/catkin_ws/devel/setup.bash; roslaunch my_robot amcl.launch"
