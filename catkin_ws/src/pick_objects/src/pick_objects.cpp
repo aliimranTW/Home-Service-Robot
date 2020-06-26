df8389d91b9e78575d1f8a4509a1389287bd3760
aliimranTW <aliimrantw@gmail.com>
2020-06-26 11:32:35 +0500

    modified pickup_objects - goal positions

--- catkin_ws/src/pick_objects/src/pick_objects.cpp
+++ catkin_ws/src/pick_objects/src/pick_objects.cpp
@@ -2,8 +2,7 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-//#include <chrono>
-//#include <thread>
+
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -15,10 +14,10 @@ int main(int argc, char** argv){
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
 
-  // set up parameter to track the robots state
+  // parameters to track robot states
   ros::NodeHandle nh;
-  nh.setParam("/isInPickupLocation", false);
-  nh.setParam("/isInDropOffLocation", false);
+  nh.setParam("/AtPickupLoc", false);
+  nh.setParam("/AtFinalLoc", false);
 
   // Wait 5 sec for move_base action server to come up
   while(!ac.waitForServer(ros::Duration(5.0))){
@@ -26,24 +25,24 @@ int main(int argc, char** argv){
   }
 
   move_base_msgs::MoveBaseGoal goal;
-  double goal_init_x =  -7;
-  double goal_init_y = -2;
-  double goal_init_w = 1.0;
+  double pickup_x =  -6;
+  double pickup_y = -2;
+  double pickup_w = 1.0;
   
-  double goal_final_x = 0;
-  double goal_final_y = 0;
-  double goal_final_w = 1.0;
+  double dest_x = 5;
+  double dest_y = -2;
+  double dest_w = 1.0;
 
   // set up the frame parameters
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = goal_init_x;
-  goal.target_pose.pose.position.y = goal_init_y;
-  goal.target_pose.pose.orientation.w = goal_init_w;
+  goal.target_pose.pose.position.x = pickup_x;
+  goal.target_pose.pose.position.y = pickup_y;
+  goal.target_pose.pose.orientation.w = pickup_w;
 
-   // Send the goal position and orientation for the robot to reach
+  // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending goal 1");
   ac.sendGoal(goal);
 
@@ -52,17 +51,17 @@ int main(int argc, char** argv){
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, home service robot reached the pickup location.");
+    ROS_INFO("Succesfully reached the pickup location");
   else
-    ROS_INFO("Failed to find the pickup location...");
+    ROS_INFO("Failed to reach the pickup location...");
 
-  // set a parameter to inform that the robot has reached the pickup location
-  nh.setParam("/isInPickupLocation", true);
+  // inform that the robot has reached the pickup location
+  nh.setParam("/AtPickupLoc", true);
 
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = goal_final_x;
-  goal.target_pose.pose.position.y = goal_final_y;
-  goal.target_pose.pose.orientation.w = goal_final_w;
+  goal.target_pose.pose.position.x = dest_x;
+  goal.target_pose.pose.position.y = dest_y;
+  goal.target_pose.pose.orientation.w = dest_w;
 
    // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending goal 2");
@@ -73,12 +72,12 @@ int main(int argc, char** argv){
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 2 meter forward");
+    ROS_INFO("Reached goal position");
   else
-    ROS_INFO("The base failed to move forward 2 meter for some reason");
+    ROS_INFO("Failed to reach goal position");
 
-  // alert that the location has been reached
-  nh.setParam("/isInDropOffLocation", true);
+  // inform that the goal location has been reached
+  nh.setParam("/AtFinalLoc", true);
 
 
   return 0;
